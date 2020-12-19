5f1f0e48bc0800cf6ce2a1960d554eb5608bc26c
VishankSingh <68466318+VishankSingh@users.noreply.github.com>
2020-12-19 18:01:03 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,21 @@
+import numpy as np
+
+training_set = np.array([[0,1,0],
+                         [0,0,1],
+                         [1,0,0],
+                         [1,1,0],
+                         [1,1,1],
+                         [0,1,1],
+                         [0,1,0]])
+
+labels = np.array([[1,
+                    0,
+                    0,
+                    1,
+                    1,
+                    0,
+                    1]])
+
+
+labels = labels.reshape(7,1)
 
